ecf6652065402ef89c52e211eb52953b21a4e5a8
Ryan Jewell <ryanjewell@eflexsystems.com>
2021-04-24 16:15:24 -0400

    noise working

--- dev/js/map.js
+++ dev/js/map.js
@@ -1,79 +1,5 @@
 import '../../node_modules/simplex-noise/simplex-noise.js';
 
-// // Aliases
-// let Application = PIXI.Application,
-//     loader = PIXI.Loader,
-//     TextureCache = PIXI.utils.TextureCache,
-//     resources = PIXI.Loader.shared.resources,
-//     Sprite = PIXI.Sprite,
-//     AnimatedSprite = PIXI.AnimatedSprite,
-//     Container = PIXI.Container,
-//     Text = PIXI.Text,
-//     TextStyle = PIXI.TextStyle,
-//     Graphics = PIXI.Graphics,
-//     Rectangle = PIXI.Rectangle,
-//     u = new SpriteUtilities(PIXI);
-
-// // Create a Pixi Application
-// PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
-
-// let app = new Application({
-//     width: 800,
-//     height: 800
-// });
-
-// // Add the canvas that Pixi automatically created for you.
-// document.body.appendChild(app.view);
-
-// loader.shared
-//     .add('../../assets/sprites.json')
-//     .load(setup);
-
-// let gameScene, sheet, id, map, state, simplex, land;
-
-// function setup() {
-// 	sheet = PIXI.Loader.shared.resources["../../assets/sprites.json"].spritesheet;
-//     id = PIXI.Loader.shared.resources['../../assets/sprites.json'].textures;
-
-// 	gameScene = new Container();
-//     app.stage.addChild(gameScene);
-
-// 	simplex = new SimplexNoise();
-
-// 	map = new Graphics();
-//     map.beginFill('0x000080');
-//     map.drawRect(0, 0, app.view.width, app.view.height);
-//     map.endFill();
-//     gameScene.addChild(map);
-
-// 	state = play;
-	
-// 	app.ticker.add(delta => gameLoop(delta));
-// }
-
-// function gameLoop(delta) {
-// 	state(delta);
-
-// 	for (var x = 0; x < 256; x++) {
-// 		for (var y = 0; y < 256; y++) {
-// 			var r = simplex.noise3D(x / 16, y / 16, delta / 16) * 0.5 + 0.5;
-// 		}
-// 	}
-
-// 	land = new Graphics();
-//     land.beginFill('0x00ff00');
-//     land.drawRect(r * app.stage.height, r * app.stage.height, 10, 10);
-//     land.endFill();
-//     gameScene.addChild(land);
-// }
-
-// function play(delta) {
-// }
-
-// function generateHeight() {
-// 	return Math.round(Math.random() * 1000);
-// }
-
 // Add browser prefixes to the requestAnimationFrame "Object Method"
 (function() {
     let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
@@ -81,30 +7,57 @@ import '../../node_modules/simplex-noise/simplex-noise.js';
     window.requestAnimationFrame = requestAnimationFrame;
 });
 
+let canvas = document.getElementById("map");
+let ctx = canvas.getContext("2d");
+
+let gen1 = new SimplexNoise();
+let gen2 = new SimplexNoise();
+let imgdata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+function noise1(nx, ny) {
+	return gen1.noise2D(nx, ny) / 2 + 0.5;
+}
+
+function noise2(nx, ny) {
+	return gen2.noise2D(nx, ny) / 2 + 0.5;
+}
+
+function changeColorsToPerlin(data) {
+	let width = canvas.width;
+	let height = canvas.height;
+	let xOctave = 1;
+	let xExp = 1.25;
+	let yOctave = 1;
+	
+	for (let x = 0; x < width; x++) {
+		for (let y = 0; y < height; y++) {
+			let nx = x/width - 0.5;
+			let ny = y/height - 0.5;
+
+			let e = (xOctave * noise1( 1 * nx, 1 * ny));
+			e = e / xOctave;
+			e = Math.pow(e, xExp);
+			
+			let m = (yOctave * noise2( 1 * nx,  1 * ny));
+			m = m / yOctave;
+
+			// Need * 4 because each pixel is += 4
+			let coordFormula = y * (width * 4) + (x * 4);
+			data[coordFormula + 0] = e / m * 255; // r
+			data[coordFormula + 1] = e / m * 255; // g
+			data[coordFormula + 2] = e / m * 255; // b
+			data[coordFormula + 3] = 255; // b
+		}
+	};
+}
+
+changeColorsToPerlin(imgdata.data);
+
+ctx.putImageData(imgdata, 0, 0);
+
 // This is just an animation cycle to see how its rendering
 function update() {
     requestAnimationFrame(update);
-
-    let canvas = document.getElementById("map");
-    let ctx = canvas.getContext("2d");
-
-	var simplex = new SimplexNoise(),
-		imgdata = ctx.getImageData(0, 0, canvas.width, canvas.height),
-		data = imgdata.data,
-		t = 0;
-	
-	for (var x = 0; x < 256; x++) {
-		for (var y = 0; y < 256; y++) {
-			var r = simplex.noise3D(x / 16, y / 16, t/16) * 0.5 + 0.5;
-			var g = simplex.noise3D(x / 8, y / 8, t/16) * 0.5 + 0.5;
-			data[(x + y * 256) * 4 + 0] = r * 255;
-			data[(x + y * 256) * 4 + 1] = (r + g) * 200;
-			data[(x + y * 256) * 4 + 2] = 0;
-			data[(x + y * 256) * 4 + 3] = 255;
-		}
-	};
-		
-	ctx.putImageData(imgdata, 0, 0);
 }
 
 update();
