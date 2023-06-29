b613d1d1e56192c0729bc33b78c01aa66ba55cbc
jewellrya <ryanjewell@eflexsystems.com>
2023-06-28 22:13:02 -0400

    got onclick for collision working kinda

--- dev/js/loot.js
+++ dev/js/loot.js
@@ -6,6 +6,8 @@ import { Ellipse, ellipseCollides } from './hurtbox.js';
 
 export let loot = [];
 let lootScale = 2.5;
+let spriteClosed, spriteOpen;
+let hurtbox;
 let player;
 
 export function lootInstance(x, y, facingDirection) {
@@ -17,8 +19,8 @@ export function lootInstance(x, y, facingDirection) {
     lootInstance.y = y;
     lootInstance.scale.set(lootScale);
     lootInstance.interactive = true;
-    let spriteClosed = getMiscSheet()['treasure-closed-' + facingDirection + '.png'];
-    let spriteOpen = getMiscSheet()['treasure-open-' + facingDirection + '.png'];
+    spriteClosed = getMiscSheet()['treasure-closed-' + facingDirection + '.png'];
+    spriteOpen = getMiscSheet()['treasure-open-' + facingDirection + '.png'];
     let sprite = new Sprite(spriteClosed);
 
     lootInstance.addChild(sprite);
@@ -26,49 +28,50 @@ export function lootInstance(x, y, facingDirection) {
     let hurtboxScale = 2;
     lootInstance.ellipse = new Ellipse(lootInstance.x + lootInstance.width / 2, lootInstance.y + lootInstance.height / 1.45, lootInstance.width / (2 / hurtboxScale), lootInstance.height / (3 / hurtboxScale));
 
-    let hurtbox = new Graphics();
+    hurtbox = new Graphics();
     hurtbox.beginFill(uiStyle.colors.red, .25);
     hurtbox.drawEllipse(sprite.x + sprite.width / 2, sprite.y + sprite.height / 1.45, sprite.width / (2 / hurtboxScale), sprite.height / (3 / hurtboxScale));
 
     lootInstance.addChild(hurtbox);
     loot.push(lootInstance);
 
-    lootInstance.on('click', function () {
-        let xShift, yShift;
-        if (facingDirection === 'D') {
-            xShift = 0 * lootScale;
-            yShift = 7 * lootScale;
-        } else {
-            xShift = 4 * lootScale;
-            yShift = 2 * lootScale;
-        }
-        if (!open) {
-            sprite.texture = spriteOpen;
-            lootInstance.y -= yShift;
-            lootInstance.x -= xShift;
-            hurtbox.y += yShift / 2.5;
-            hurtbox.x += xShift / 2.5;
-            open = true;
-        } else {
-            sprite.texture = spriteClosed;
-            open = false;
-            lootInstance.y += yShift;
-            lootInstance.x += xShift;
-            hurtbox.y -= yShift / 2.5;
-            hurtbox.x -= xShift / 2.5;
-        }
-    })
+    
 
     return lootInstance;
 }
 
 export function lootCollide_listener() {
-    loot.forEach(derp => {
-        if (ellipseCollides(player.ellipse, derp.ellipse)) {
-            console.log('collide');
+    loot.forEach(loot => {
+        let sprite = loot.children[0];
+        if (ellipseCollides(player.ellipse, loot.ellipse)) {
+            loot.on('click', function () {
+                // let xShift, yShift;
+                // let facingDirection = 'U';
+                // if (facingDirection === 'D') {
+                //     xShift = 0 * lootScale;
+                //     yShift = 7 * lootScale;
+                // } else {
+                //     xShift = 4 * lootScale;
+                //     yShift = 2 * lootScale;
+                // }
+                if (!open) {
+                    sprite.texture = spriteOpen;
+                    // lootInstance.y -= yShift;
+                    // lootInstance.x -= xShift;
+                    // hurtbox.y += yShift / 2.5;
+                    // hurtbox.x += xShift / 2.5;
+                    open = true;
+                } else {
+                    sprite.texture = spriteClosed;
+                    open = false;
+                    // lootInstance.y += yShift;
+                    // lootInstance.x += xShift;
+                    // hurtbox.y -= yShift / 2.5;
+                    // hurtbox.x -= xShift / 2.5;
+                }
+            })
+        } else {
+            loot.removeAllListeners();
         }
     })
-    // if(ellipseCollides(player.ellipse, sprite.ellipse)) {
-    //     console.log('collide');
-    // }
 }
